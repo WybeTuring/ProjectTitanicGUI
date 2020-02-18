d7e1ac362776dd8dafbe03e3c9bd35ec482644f2
Karl Lemfon <karllemfon@gmail.com>
2020-02-18 22:05:16 +0000

    More controllers and validation.

--- src/titanic/app/address/view/galamseyViewController.java
+++ src/titanic/app/address/view/galamseyViewController.java
@@ -139,7 +139,7 @@ public class galamseyViewController {
 			alert1.showAndWait();
 			vegetationColourField.setText("");
     	}
-    	else if(!(colourValue != "1" || colourValue != "2" || colourValue != "3")) {
+    	else if(!(Double.parseDouble(colourValue) == 1 || colourValue == "2" || colourValue == "3")) {
     		Alert alert1 = new Alert(AlertType.NONE);
 			alert1.setAlertType(AlertType.ERROR);
 			alert1.setContentText("Colour Values can only be 1, 2, or 3");
