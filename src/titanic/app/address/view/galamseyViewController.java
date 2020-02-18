7cf2c670872240e71c1552196e217cdf33ec0103
Karl Lemfon <karllemfon@gmail.com>
2020-02-18 21:18:53 +0000

    More validation on the page for registering the galamsey objects

--- src/titanic/app/address/view/galamseyViewController.java
+++ src/titanic/app/address/view/galamseyViewController.java
@@ -7,8 +7,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -80,6 +82,15 @@ public class galamseyViewController {
         }
     }
     
+    /**
+	 * Checks if a string is a number and using that to display a warning.
+	 * @param str
+	 * @return
+	 */
+    private boolean isNumeric(String str) {
+		  return str.matches("-?\\d+(\\.\\d+)?"); 
+		}
+    
     public void newButtonClick(MouseEvent event) {
     	
     	String observatoryName = observatoryNameField.getText();
@@ -87,6 +98,81 @@ public class galamseyViewController {
     	String colourValue = colourValueField.getText();
     	String latitude = latitudeField.getText();
     	String longitude = longitudeField.getText();
+    	
+    	
+    	
+    	// Validating the user input from the register Galamsey page
+    	
+    	if(observatoryName.equals("") || vegetationColour.equals("") || colourValue.equals("") || latitude.equals("") || longitude.equals("")) {
+    		Alert alert = new Alert(AlertType.NONE);
+			alert.setAlertType(AlertType.ERROR);
+			alert.setContentText("Please enter all fields.");
+			alert.showAndWait();
+			if(!observatoryName.equals("")) {
+				observatoryCheckbox.setSelected(true);
+			}
+			if(!vegetationColour.equals("") && (vegetationColour.toLowerCase().equals("yellow") || vegetationColour.toLowerCase().equals("green") || vegetationColour.toLowerCase().equals("brown") )) {
+				vegetationCheckbox.setSelected(true);
+			}
+			else {
+				Alert alert1 = new Alert(AlertType.NONE);
+				alert1.setAlertType(AlertType.ERROR);
+				alert1.setContentText("Vegetation colours can only be green, yellow and brown.");
+				alert1.showAndWait();
+				vegetationColourField.setText("");
+			}
+			if(!colourValue.equals("") && (colourValue != "1" || colourValue != "2" || colourValue != "3") ){
+				Alert alert1 = new Alert(AlertType.NONE);
+				alert1.setAlertType(AlertType.ERROR);
+				alert1.setContentText("Colour Values can only be 1, 2, or 3");
+				alert1.showAndWait();
+				colourValueField.setText("");
+			}
+			else {
+				colourCheckbox.setSelected(true);
+			}
+    	}
+    	else if(!(vegetationColour.toLowerCase().equals("yellow") || vegetationColour.toLowerCase().equals("green") || vegetationColour.toLowerCase().equals("brown") )) {
+    		Alert alert1 = new Alert(AlertType.NONE);
+			alert1.setAlertType(AlertType.ERROR);
+			alert1.setContentText("Vegetation colours can only be green, yellow and brown.");
+			alert1.showAndWait();
+			vegetationColourField.setText("");
+    	}
+    	else if(!(colourValue != "1" || colourValue != "2" || colourValue != "3")) {
+    		Alert alert1 = new Alert(AlertType.NONE);
+			alert1.setAlertType(AlertType.ERROR);
+			alert1.setContentText("Colour Values can only be 1, 2, or 3");
+			alert1.showAndWait();
+			colourValueField.setText("");
+    	}
+    	else if(!isNumeric(latitude) || !isNumeric(longitude) ) {
+    		Alert alert1 = new Alert(AlertType.NONE);
+			alert1.setAlertType(AlertType.ERROR);
+			alert1.setContentText("Latitude and Longitude must be numbers");
+			alert1.showAndWait();
+			latitudeField.setText("");
+			longitudeField.setText("");
+    	}
+    	// The block in which everything has been validated
+    	else {
+    		colourCheckbox.setSelected(true);
+    		observatoryCheckbox.setSelected(true);
+    		vegetationCheckbox.setSelected(true);
+    		longitudeCheckbox.setSelected(true);
+    		latitudeCheckbox.setSelected(true);
+    		
+    		// Process the values obtained
+    		
+    		
+    		// Clear the fields
+    		vegetationColourField.setText("");
+    		latitudeField.setText("");
+			longitudeField.setText("");
+			colourValueField.setText("");
+			observatoryNameField.setText("");
+    		
+    	}
     }
 
 }
